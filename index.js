22ea14a88416f21e5c6204a01489b93334c5e8b8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-05 15:09:52 +0530

    updated packages

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
@@ -18,7 +36,7 @@ app.post("/teas", (req, res) => {
 });
 
 // get all Teas
-app.get("/teas", (req, res) => {
+app.get("/teas", (_, res) => {
   res.status(200).send(teaData);
 });
 
